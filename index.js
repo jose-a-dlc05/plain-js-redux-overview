af6cd71741ef1b59df0bb465bdf4099a0c3a197e
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-10-09 17:58:34 -0400

    Added feature to remove from favorite things array

--- index.js
+++ index.js
@@ -8,13 +8,46 @@ function changeCount(amount) {
 	};
 }
 
+function addFavoriteThing(thing) {
+	return {
+		type: 'ADD_FAVORITE_THING',
+		data: thing,
+	};
+}
+
+function removeFavoriteThing(thing) {
+	return {
+		type: 'REMOVE_FAVORITE_THING',
+		data: thing,
+	};
+}
+
+const initialState = {
+	count: 0,
+	favoriteThings: [],
+};
+
 // 2. Reducer to handle these actions
-function reducer(state = { count: 0 }, action) {
+function reducer(state = initialState, action) {
 	// return new state based on the incoming action.type
 	switch (action.type) {
 		case 'CHANGE_COUNT':
 			return {
+				...state,
 				count: state.count + action.data,
+			};
+		case 'ADD_FAVORITE_THING':
+			return {
+				...state,
+				favoriteThings: [...state.favoriteThings, action.data],
+			};
+		case 'REMOVE_FAVORITE_THING':
+			const updatedArr = state.favoriteThings.filter(
+				(thing) => thing.toLowerCase() !== action.data.toLowerCase()
+			);
+			return {
+				...state,
+				favoriteThings: updatedArr,
 			};
 		default:
 			return state;
@@ -28,6 +61,6 @@ store.subscribe(() => {
 });
 
 store.dispatch(changeCount(5));
-store.dispatch(changeCount(5));
-store.dispatch(changeCount(5 * 2));
-store.dispatch(changeCount(5));
+store.dispatch(addFavoriteThing('Gorons'));
+store.dispatch(addFavoriteThing('Zora'));
+store.dispatch(removeFavoriteThing('Zora'));
