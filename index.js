1f79d28e26e56cb7170f42c1ef2886ca103a67ab
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-10-09 17:16:27 -0400

    added the payload or data property to action creator

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
 const redux = require('redux');
 
-// 1. Action creators for increment, decrement, double and halve
-function increment() {
+// 1. Action creators for changing count
+function changeCount(amount) {
 	return {
-		type: 'INCREMENT',
-	};
-}
-
-function decrement() {
-	return {
-		type: 'DECREMENT',
-	};
-}
-
-function double() {
-	return {
-		type: 'DOUBLE',
-	};
-}
-
-function halve() {
-	return {
-		type: 'HALVE',
+		type: 'CHANGE_COUNT',
+		data: amount,
 	};
 }
 
@@ -29,32 +12,22 @@ function halve() {
 function reducer(state = { count: 0 }, action) {
 	// return new state based on the incoming action.type
 	switch (action.type) {
-		case 'INCREMENT':
+		case 'CHANGE_COUNT':
 			return {
-				count: state.count + 1,
-			};
-		case 'DECREMENT':
-			return {
-				count: state.count - 1,
-			};
-		case 'DOUBLE':
-			return {
-				count: state.count * 2,
-			};
-		case 'HALVE':
-			return {
-				count: Math.floor(state.count / 2),
+				count: state.count + action.data,
 			};
 		default:
 			return state;
 	}
 }
 
-// ACTIONS are an object
-// ACTIONS require a property called "type", which by convention requires a capitalized string and if words are separated you would do so with an underscore
-// The value is a string description of the kind of change you want to make to state
-// ACTIONS are objects that tells your reducer what you're trying to do(type) and what info it needs to do that(payload), what your reducer is operating on and using it to help return new state
+// 3. Create the store
+const store = redux.createStore(reducer); // returns object with dispatch, subscribe and getState properties
+store.subscribe(() => {
+	console.log(store.getState());
+});
 
-// ACTION CREATORS are an action object creator function
-
-// REDUCERS take in state and that action, depending on the type, its going to tell it what to do
+store.dispatch(changeCount(5));
+store.dispatch(changeCount(5));
+store.dispatch(changeCount(5 * 2));
+store.dispatch(changeCount(5));
