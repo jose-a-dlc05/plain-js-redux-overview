ba8af7bb83b759965fc1c459df47010ad59e02d2
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-10-09 16:19:19 -0400

    Added action creators, reducer to the file

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const redux = require('redux');
-const initialState = {
-	count: 0,
-};
 
-function reducer(state = initialState, action) {
+// 1. Action creators for increment, decrement, double and halve
+function increment() {
+	return {
+		type: 'INCREMENT',
+	};
+}
+
+function decrement() {
+	return {
+		type: 'DECREMENT',
+	};
+}
+
+function double() {
+	return {
+		type: 'DOUBLE',
+	};
+}
+
+function halve() {
+	return {
+		type: 'HALVE',
+	};
+}
+
+// 2. Reducer to handle these actions
+function reducer(state = { count: 0 }, action) {
+	// return new state based on the incoming action.type
 	switch (action.type) {
 		case 'INCREMENT':
 			return {
@@ -13,16 +37,24 @@ function reducer(state = initialState, action) {
 			return {
 				count: state.count - 1,
 			};
+		case 'DOUBLE':
+			return {
+				count: state.count * 2,
+			};
+		case 'HALVE':
+			return {
+				count: Math.floor(state.count / 2),
+			};
 		default:
 			return state;
 	}
 }
 
-const store = redux.createStore(reducer);
-store.subscribe(() => {
-	console.log(store.getState());
-});
+// ACTIONS are an object
+// ACTIONS require a property called "type", which by convention requires a capitalized string and if words are separated you would do so with an underscore
+// The value is a string description of the kind of change you want to make to state
+// ACTIONS are objects that tells your reducer what you're trying to do(type) and what info it needs to do that(payload), what your reducer is operating on and using it to help return new state
 
-store.dispatch({ type: 'INCREMENT' });
-store.dispatch({ type: 'INCREMENT' });
-store.dispatch({ type: 'DECREMENT' });
+// ACTION CREATORS are an action object creator function
+
+// REDUCERS take in state and that action, depending on the type, its going to tell it what to do
